6740dc101997592ffae1488a470b5164f3385965
GugaSantos <guga.santos.guga@gmail.com>
2022-02-18 16:04:15 -0300

    Requisito 10 ok e correçoes ortograficas

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
     return (
-      <div>
+      <div data-testid="missions">
         <Title headline="Missões" />
+        {missions.map(({ name, year, country, destination }) => (
+          <MissionCard
+            name={ name }
+            year={ year }
+            country={ country }
+            destination={ destination }
+            key={ name }
+          />
+        ))}
       </div>
     );
   }
--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -8,7 +8,7 @@ class PlanetCard extends Component {
     return (
       <div data-testid="planet-card">
         <p data-testid="planet-name">{ planetName }</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName} ` } />
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
