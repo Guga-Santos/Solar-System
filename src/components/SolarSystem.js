c9f7594b1664fb6e352a7a4fb37673387bf4f2c0
GugaSantos <guga.santos.guga@gmail.com>
2022-02-18 14:52:09 -0300

    Requisito 02 ok

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SolarSystem extends Component {
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <p>Teste</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
