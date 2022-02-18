bd643c3c471e6cc44ab0b378dee72870acd00df9
GugaSantos <guga.santos.guga@gmail.com>
2022-02-18 15:31:10 -0300

    Requisito 06 ok e correçoes no 04

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends Component {
   render() {
@@ -12,5 +13,10 @@ class PlanetCard extends Component {
     );
   }
 }
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Title from './Title';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
-        <p>Teste</p>
+        <Title headline="Planetas" />
+        {planets.map(({ name, image }) => (
+          <PlanetCard planetName={ name } planetImage={ image } key={ name } />
+        ))}
       </div>
     );
   }
